bae4ba5c2a1381209651c9f8e092ab6890f68786
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2023-12-20 15:16:34 +0530

    Fix configuration

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,12 @@ public class Main {
         Profile mp = new ProfileImpl();
         mp.setParameter(Profile.MAIN_HOST, "localhost");
         mp.setParameter(Profile.GUI, "true");
-        mp.setParameter(Profile.MAIN_PORT, "1002");
-        mp.setParameter(Profile.PLATFORM_ID, "P1");
+        mp.setParameter(Profile.MAIN_PORT, "1099");
         ContainerController mcc = rt.createMainContainer(mp);
-//         Create Containers and Agents
-        Profile cp1 = new ProfileImpl();
-        cp1.setParameter(Profile.MAIN_HOST, "localhost");
-        cp1.setParameter(Profile.GUI, "true");
-        ContainerController cc1 = rt.createAgentContainer(cp1);
         for (int i = 0; i <= 9; i++) {
             AgentController ac;
             try {
                 ac = mcc.createNewAgent("Agent0" + i, "FirstAgent", null);
-                ac.start();
-                ac = cc1.createNewAgent("Agent1" + i, "FirstAgent", null);
                 ac.start();
             } catch (StaleProxyException e) {
                 throw new RuntimeException(e);
